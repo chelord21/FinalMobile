e8fdcbb14ebc060e4084f2e52487415a4e7ef2a4
MarcoRmz <marcormz19@gmail.com>
2015-04-19 11:55:12 -0500

    added edit image to profile

--- app/src/main/java/itesm/mx/finalprojectmobile20/userProfile.java
+++ app/src/main/java/itesm/mx/finalprojectmobile20/userProfile.java
@@ -1,7 +1,18 @@
 package itesm.mx.finalprojectmobile20;
 
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +23,11 @@ import android.widget.Toast;
 
 import com.parse.ParseUser;
 
+import java.io.File;
+
 
 public class UserProfile extends ActionBarActivity {
+    private static final String TAG = "";
 
     //TextViews
     /*
@@ -28,6 +42,7 @@ public class UserProfile extends ActionBarActivity {
 
     //Buttons
     Button userProfile_back_Btn;
+    Button userProfile_edit_Btn;
 
     //ImageView
     ImageView userProfile_profile_IV;
@@ -42,7 +57,7 @@ public class UserProfile extends ActionBarActivity {
         setContentView(R.layout.activity_user_profile);
         userProfile_username_TV = (TextView) findViewById(R.id.userProf_username_TV);
         userProfile_email_TV = (TextView) findViewById(R.id.userProf_email_TV);
-
+        userProfile_edit_Btn = (Button) findViewById(R.id.userProf_edit_Btn);
         userProfile_profile_IV = (ImageView) findViewById(R.id.userProf_profile_IV);
         userProfile_username_TV.setText(ParseUser.getCurrentUser().getUsername());
         userProfile_email_TV.setText(ParseUser.getCurrentUser().getEmail());
@@ -52,9 +67,89 @@ public class UserProfile extends ActionBarActivity {
                 if(userProfile_back_Btn.isPressed()){
                     Toast.makeText(getApplicationContext(), "Works", Toast.LENGTH_SHORT).show();
                 }
+                else if (userProfile_edit_Btn.isPressed()) {
+                    final CharSequence[] options = { "Take Photo", "Select from Gallery","Cancel" };
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(UserProfile.this);
+                    builder.setTitle("Edit Photo");
+                    builder.setItems(options, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int item) {
+                            if (options[item].equals("Take Photo")) {
+                                try {
+                                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "profile.jpg");
+
+                                    //if (f.exists() && f.canWrite()) f.delete();
+
+                                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                                    intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+
+                                    startActivityForResult(intent, 1);
+                                }
+                                catch (ActivityNotFoundException e) {
+                                    Log.e(TAG, "No camera: " + e);
+                                }
+                                catch ( Exception e ) {
+                                    Log.e(TAG, "Cannot make photo: " + e);
+                                }
+                            }
+                            else if (options[item].equals("Select from Gallery")) {
+                                try {
+                                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                                    intent.setType("image/*");
+                                    startActivityForResult(intent, 2);
+                                }
+                                catch (ActivityNotFoundException e) {
+                                    Log.e(TAG, "No gallery: " + e);
+                                }
+                            }
+                            else if (options[item].equals("Cancel")) {
+                                dialog.dismiss();
+                            }
+                        }
+                    });
+                    builder.show();
+                }
             }
         };
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                File f = new File(Environment.getExternalStorageDirectory().toString());
+                for (File temp : f.listFiles()) {
+                    if (temp.getName().equals("profile.jpg")) {
+                        f = temp;
+                        break;
+                    }
+                }
+                try {
+                    Bitmap bitmap;
+                    BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                            bitmapOptions);
+
+                    userProfile_profile_IV.setImageBitmap(bitmap);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == 2) {
+                Uri selectedImage = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                userProfile_profile_IV.setImageBitmap(thumbnail);
+            }
+        }
     }
 
     @Override
