69bcc89e309d95cad0c629e92e8112434dc7832e
Marcelo Salcedo <salcedogzz.jm@gmail.com>
2015-04-30 16:54:03 -0500

    Modified Chat, Chat_Main and Chat_Message layouts. Deleted layout not used called Chat_Item. Modified ChatListAdapter class

--- app/src/main/java/itesm/mx/finalprojectmobile20/chat/ChatListAdapter.java
+++ app/src/main/java/itesm/mx/finalprojectmobile20/chat/ChatListAdapter.java
@@ -2,6 +2,8 @@ package itesm.mx.finalprojectmobile20.chat;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -25,13 +27,20 @@ public class ChatListAdapter extends FirebaseListAdapter<Chat>  {
         // Map a Chat object to an entry in our listview
         String author = chat.getAuthor();
         TextView authorText = (TextView) view.findViewById(R.id.author);
-        authorText.setText(author + ": ");
+        authorText.setText(author);
         // If the message was sent by this user, color it differently
         if (author != null && author.equals(chatUser)) {
+            Log.d("Prueba de tardanza coloreado author", "Se coloreo usuario rojo");
             authorText.setTextColor(Color.RED);
+            authorText.setGravity(Gravity.RIGHT);
+            ((TextView) view.findViewById(R.id.message)).setText(chat.getMessage());
+            ((TextView) view.findViewById(R.id.message)).setGravity(Gravity.RIGHT);
         } else {
+            Log.d("Prueba de tardanza coloreado author", "Se coloreo usuario azul");
             authorText.setTextColor(Color.BLUE);
+            authorText.setGravity(Gravity.LEFT);
+            ((TextView) view.findViewById(R.id.message)).setText(chat.getMessage());
+            ((TextView) view.findViewById(R.id.message)).setGravity(Gravity.LEFT);
         }
-        ((TextView) view.findViewById(R.id.message)).setText(chat.getMessage());
     }
 }
