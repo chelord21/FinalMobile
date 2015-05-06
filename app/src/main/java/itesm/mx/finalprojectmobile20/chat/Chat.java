ae23a49615fffb41da98f74b35b689488acb8027
Alejandro Sanchez <alejandro.s1993@gmail.com>
2015-05-06 16:49:46 -0500

    App works, full functional

--- app/src/main/java/itesm/mx/finalprojectmobile20/chat/Chat.java
+++ app/src/main/java/itesm/mx/finalprojectmobile20/chat/Chat.java
@@ -4,24 +4,24 @@ package itesm.mx.finalprojectmobile20.chat;
  * Created by AlejandroSanchez on 4/22/15.
  */
 public class Chat {
-    private String chat_message;
-    private String chat_author;
+    private String message;
+    private String author;
 
     // Required default constructor for Firebase object mapping
     @SuppressWarnings("unused")
     private Chat() {
     }
 
-    Chat(String chat_message, String chat_author) {
-        this.chat_message = chat_message;
-        this.chat_author = chat_author;
+    Chat(String message, String author) {
+        this.message = message;
+        this.author = author;
     }
 
     public String getMessage() {
-        return chat_message;
+        return message;
     }
 
     public String getAuthor() {
-        return chat_author;
+        return author;
     }
 }
