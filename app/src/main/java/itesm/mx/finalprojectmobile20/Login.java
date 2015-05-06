1ca4b0c74f9a70679564e263c8268266840fb7e8
Marcelo Salcedo <salcedogzz.jm@gmail.com>
2015-05-06 18:46:09 -0500

    Erased unnecesary code from Login class

--- app/src/main/java/itesm/mx/finalprojectmobile20/Login.java
+++ app/src/main/java/itesm/mx/finalprojectmobile20/Login.java
@@ -169,23 +169,6 @@ public class Login extends ActionBarActivity {
         });
     }
 
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private boolean isNetworkConnected() {
         ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo ni = cm.getActiveNetworkInfo();
